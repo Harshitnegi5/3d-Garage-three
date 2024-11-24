489894646251f19197e6f8f4b71e2722f13cbc00
Harshit Negi <harshitnegi50@gmail.com>
2024-11-24 21:22:43 +0530

    add glow effect

--- script.js
+++ script.js
@@ -6,39 +6,53 @@ const camera = new THREE.PerspectiveCamera(
   window.innerWidth / window.innerHeight,
   0.1,
   1000
-);
-const renderer = new THREE.WebGLRenderer({ antialias: true });
-renderer.setSize(window.innerWidth, window.innerHeight);
+);  
+
+camera.position.set(60,10,50);
+camera.lookAt(0,0,0 )
+
+const renderer = new THREE.WebGLRenderer({
+   antialias: true,
+   powerPerformance : "high-performance"
+   });
+renderer.setSize(window.innerWidth, window.innerHeight);   
+renderer.setPixelRatio(Math.min(window.devicePixelRatio,2))
+renderer.toneMapping = THREE.ACESFilmicToneMapping;
+renderer.toneMappingExposure = .75;
 document.querySelector(".model").appendChild(renderer.domElement);
 
 
 const controls = new THREE.OrbitControls(camera, renderer.domElement);
+controls.enableDamping =true;
+controls.dampingFactor = .05;
+controls.minDistance = 10;
+controls.maxDistance = 50;
+controls.maxPolarAngle = Math.PI/2;
+
+
+const createEmissonMaterial = (color , intensity=2)=>{
+  return new THREE.MeshStandardMaterial({
+    color:color,
+    emission:color,
+    emissionIntensity : intensity,
+    toneMapped : false
+  })
+  
+}
 
 let model;
 
 let loader = new THREE.GLTFLoader();
 loader.load("./assets/scene.gltf", (gltf) => {
   model = gltf.scene;
+
+  const box = new THREE.Box3().setFromObject(model
+  );
+  const center = box.getCenter(new THREE.Vector3())
+  model.position.sub(center)
   scene.add(model);
-});
-
-const canvasContainer = renderer.domElement;
-
-// Initially set the cursor to grab
-canvasContainer.style.cursor = 'grab';
-
-// Detect when the user starts dragging
-controls.addEventListener('start', () => {
-    canvasContainer.style.cursor = 'grabbing'; // Change to grabbing cursor
-});
-
-// Detect when the user stops dragging
-controls.addEventListener('end', () => {
-    canvasContainer.style.cursor = 'grab'; // Change back to grab cursor
-});
-
-
-const ambientLight = new THREE.AmbientLight(0x404040, 1);
+});  
+const ambientLight = new THREE.AmbientLight(0xffffff, 0);
 scene.add(ambientLight);
 
 const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
@@ -57,15 +71,41 @@ spotLight.angle = Math.PI / 4;
 spotLight.penumbra = 0.1;
 scene.add(spotLight);
 
+const composer = new THREE.EffectComposer(renderer);
+const renderPass = new THREE.RenderPass(scene,camera)
+composer.addPass(renderPass)
+
+const bloom = new THREE.UnrealBloomPass(
+  new THREE.Vector2(window.innerWidth , window.innerHeight),
+  .6,
+  1,
+  .1,
+
+)
+
+composer.addPass(bloom)
+
+const canvasContainer = renderer.domElement;
+
+canvasContainer.style.cursor = 'grab';
 
 
-camera.position.set(60,10,10);
-camera.lookAt(0,0,0 )
+controls.addEventListener('start', () => {
+    canvasContainer.style.cursor = 'grabbing'; 
+});    
+
+
+controls.addEventListener('end', () => {
+    canvasContainer.style.cursor = 'grab'; 
+});    
+
+
+
 
 function animate() {
   requestAnimationFrame(animate);
   controls.update(); 
-  renderer.render(scene, camera);
+  composer.render()
 }
 
 animate();
